c5b881b44e39fa3f16ea719c7c38cb9611d94b3c
ucan-lab <35098175+ucan-lab@users.noreply.github.com>
2018-10-13 18:21:05 +0900

    user seeding

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+        factory(User::class, 30)->create();
+    }
+}
