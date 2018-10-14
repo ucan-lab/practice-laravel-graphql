7a9abd13fd5c0777dd5ef19ba34735a43a56012d
ucan-lab <35098175+ucan-lab@users.noreply.github.com>
2018-10-14 11:43:05 +0900

    add UserQuery

--- app/GraphQL/Query/UserQuery.php
+++ app/GraphQL/Query/UserQuery.php
@@ -1,33 +1,71 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\GraphQL\Query;
 
 use Folklore\GraphQL\Support\Query;
 use GraphQL\Type\Definition\ResolveInfo;
 use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Definition\ObjectType;
 use GraphQL;
+use App\User;
 
+/**
+ * Userクエリの定義
+ */
 class UserQuery extends Query
 {
+    /**
+     * クエリ名の定義と概要
+     *
+     * @var array
+     */
     protected $attributes = [
-        'name' => 'UserQuery',
-        'description' => 'A query'
+        'name' => 'user',
+        'description' => 'user query'
     ];
 
-    public function type()
+    /**
+     * クエリが扱う型を定義
+     *
+     * @return ObjectType
+     */
+    public function type() : ObjectType
     {
-        return Type::listOf(Type::string());
+        return GraphQL::type('UserType');
     }
 
-    public function args()
+    /**
+     * クエリが取り得る引数を定義
+     *
+     * @return array
+     */
+    public function args() : array
     {
         return [
-            
+            'id' => [
+                'name' => 'id',
+                'type' => Type::id(),
+            ],
         ];
     }
 
-    public function resolve($root, $args, $context, ResolveInfo $info)
+    /**
+     * クエリに対する実処理
+     *
+     * @param array $root
+     * @param array $args
+     * @return User
+     */
+    public function resolve($root, $args, $context, ResolveInfo $info) : User
     {
-        return [];
+        $query = User::query();
+
+        if (isset($args['id'])) {
+            $query->where('id', $args['id']);
+        }
+
+        return $query->first();
     }
 }
--- config/graphql.php
+++ config/graphql.php
@@ -124,7 +124,7 @@ return [
     'schemas' => [
         'default' => [
             'query' => [
-
+                App\GraphQL\Query\UserQuery::class,
             ],
             'mutation' => [
 
