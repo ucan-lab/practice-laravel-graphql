e91b3295505d2ee9a80ca1b3e37785162fab749d
ucan-lab <35098175+ucan-lab@users.noreply.github.com>
2018-10-17 16:25:47 +0900

    php artisan make:graphql:type PostType

---
+++ app/GraphQL/Type/PostType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\GraphQL\Type;
+
+use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Type as BaseType;
+use GraphQL;
+
+class PostType extends BaseType
+{
+    protected $attributes = [
+        'name' => 'PostType',
+        'description' => 'A type'
+    ];
+
+    public function fields()
+    {
+        return [
+            
+        ];
+    }
+}
