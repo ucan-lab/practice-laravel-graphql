9f4f34c79e4ffde02b1cfacba2f16794107a9fac
ucan-lab <35098175+ucan-lab@users.noreply.github.com>
2018-10-17 16:44:50 +0900

    php artisan make:graphql:query PostsQuery

---
+++ app/GraphQL/Query/PostsQuery.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\GraphQL\Query;
+
+use Folklore\GraphQL\Support\Query;
+use GraphQL\Type\Definition\ResolveInfo;
+use GraphQL\Type\Definition\Type;
+use GraphQL;
+
+class PostsQuery extends Query
+{
+    protected $attributes = [
+        'name' => 'PostsQuery',
+        'description' => 'A query'
+    ];
+
+    public function type()
+    {
+        return Type::listOf(Type::string());
+    }
+
+    public function args()
+    {
+        return [
+            
+        ];
+    }
+
+    public function resolve($root, $args, $context, ResolveInfo $info)
+    {
+        return [];
+    }
+}
