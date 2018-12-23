f104655b5cbf02aba60c5cee1c6e3b8dfce86979
ucan-lab <35098175+ucan-lab@users.noreply.github.com>
2018-12-24 00:34:54 +0900

    add 記事を登録するテスト

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,18 +4,35 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic test example.
-     *
+     * @test
      * @return void
      */
-    public function testBasicTest()
+    public function 記事を登録するテスト()
     {
-        $response = $this->get('/');
+        $query = file_get_contents(__DIR__ . '/CreatePostMutation.gql');
 
-        $response->assertStatus(200);
+        $user = factory(User::class)->create();
+
+        $variables = [
+            'user_id' => $user->id,
+            'title' => 'テストタイトル',
+            'content' => 'テスト内容',
+        ];
+
+        $json = [
+            'data' => [
+                'CreatePost' => $variables
+            ],
+        ];
+
+        $response = $this->graphql($query, $variables);
+        $response->assertStatus(200)->assertJson($json);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,28 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\TestResponse;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * GraphQL POST request.
+     *
+     * @param  string $query
+     * @param  array  $variables
+     * @param  array  $data
+     * @param  array  $headers
+     * @return \Illuminate\Foundation\Testing\TestResponse
+     */
+    protected function graphql(string $query, array $variables = [], array $data = [], array $headers = []): TestResponse
+    {
+        $data = [
+            'query' => $query,
+            'variables' => json_encode($variables),
+        ] + $data;
+
+        return $this->post('/graphql', $data, $headers);
+    }
 }
